b1973a4df67cb52ffc9dcfe4d86a72904b01d6dc
qdrs <me@qdrs.dev>
2023-08-18 15:47:03 +0100

    export initspace macro

--- solana/anchor/src/lib.rs
+++ solana/anchor/src/lib.rs
@@ -28,7 +28,7 @@ pub mod prelude {
     };
 
     pub use otter_solana_macro::{
-        account, declare_id, error_code, invariant, program, Accounts, Arbitrary,
+        account, declare_id, error_code, invariant, program, Accounts, Arbitrary, InitSpace,
     };
 
     pub use crate::account::{self, Account};

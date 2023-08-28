df60df72b95c4f58d0ff86fc7ddc1e015a6e06ca
qdrs <me@qdrs.dev>
2023-08-28 14:15:33 +0800

    update rent

--- program/src/rent.rs
+++ program/src/rent.rs
@@ -1,5 +1,13 @@
-pub const ACCOUNT_STORAGE_OVERHEAD: u64 = 128;
+use crate::error::Error;
 
+pub const ACCOUNT_STORAGE_OVERHEAD: u64 = 128;
+pub const DEFAULT_LAMPORTS_PER_BYTE_YEAR: u64 = 1_000_000_000 / 100 * 365 / (1024 * 1024);
+pub const DEFAULT_EXEMPTION_THRESHOLD: f64 = 2.0;
+pub const DEFAULT_BURN_PERCENT: u8 = 50;
+
+/// Account storage overhead for calculation of
+
+#[derive(Clone, Copy, Debug)]
 pub struct Rent {
     /// Rental rate
     pub lamports_per_byte_year: u64,
@@ -11,6 +19,16 @@ pub struct Rent {
     pub burn_percent: u8,
 }
 
+impl Default for Rent {
+    fn default() -> Self {
+        Self {
+            lamports_per_byte_year: DEFAULT_LAMPORTS_PER_BYTE_YEAR,
+            exemption_threshold: DEFAULT_EXEMPTION_THRESHOLD,
+            burn_percent: DEFAULT_BURN_PERCENT,
+        }
+    }
+}
+
 impl Rent {
     pub fn minimum_balance(&self, data_len: usize) -> u64 {
         let bytes = data_len as u64;
@@ -20,6 +38,10 @@ impl Rent {
 
     pub fn is_exempt(&self, balance: u64, data_len: usize) -> bool {
         balance >= self.minimum_balance(data_len)
+    }
+
+    pub fn get() -> Result<Self, Error> {
+        Ok(Self::default())
     }
 }
 

7c959a16609054bf6fc5f50f0de4869afd9e4119
qdrs <me@qdrs.dev>
2023-08-31 16:08:04 +0800

    interfaces

--- anchor_spl/src/token_interface.rs
+++ anchor_spl/src/token_interface.rs
@@ -6,3 +6,17 @@ pub struct Mint;
 impl AccountDeserialize for Mint {}
 
 impl AccountSerialize for Mint {}
+
+#[derive(Clone, Debug, Default, PartialEq, AnchorDeserialize, AnchorSerialize)]
+pub struct TokenAccount;
+
+impl AccountDeserialize for TokenAccount {}
+
+impl AccountSerialize for TokenAccount {}
+
+#[derive(Clone, Debug, Default, PartialEq, AnchorDeserialize, AnchorSerialize)]
+pub struct TokenInterface;
+
+impl AccountDeserialize for TokenInterface {}
+
+impl AccountSerialize for TokenInterface {}

e0fdd334d15cfae5a9a36146713ce914d8c779a7
swaroop-osec <swaroop@osec.io>
2023-09-12 22:19:10 +0530

    Add new macro helper_fn

---
+++ macro_core/src/helper.rs
@@ -0,0 +1,17 @@
+#[cfg(feature = "anchor")]
+use {anyhow::Result, proc_macro2::TokenStream, quote::quote, syn::ItemFn};
+
+#[cfg(feature = "anchor")]
+use crate::program::remove_verify_ignore_statements;
+
+#[cfg(feature = "anchor")]
+pub fn access_control(input: TokenStream) -> Result<TokenStream> {
+    let Ok(mut item) = syn::parse2::<ItemFn>(input) else {
+        panic!("use #[helper] on a function")
+    };
+    remove_verify_ignore_statements(&mut item);
+
+    Ok(quote! {
+        #item
+    })
+}

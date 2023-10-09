53a217236dbdc01b53ee3c2f69c2fa08d4946aca
swaroop-osec <swaroop@osec.io>
2023-10-09 09:47:20 +0530

    Kani Arbitrary for AddressLookup tables

--- address_lookup_table/src/state.rs
+++ address_lookup_table/src/state.rs
@@ -8,6 +8,9 @@ pub const LOOKUP_TABLE_MAX_ADDRESSES: usize = 256;
 /// The serialized size of lookup table metadata
 pub const LOOKUP_TABLE_META_SIZE: usize = 56;
 
+#[cfg(any(kani, feature = "kani"))]
+use solana_program::pubkey::kani_new_pubkey;
+
 #[derive(Debug, PartialEq, Eq, Clone, Default, BorshDeserialize, BorshSerialize)]
 pub struct LookupTableMeta {
     /// Lookup tables cannot be closed until the deactivation slot is
@@ -28,8 +31,31 @@ pub struct LookupTableMeta {
     // the account's data, starting from `LOOKUP_TABLE_META_SIZE`.
 }
 
+#[cfg(any(kani, feature = "kani"))]
+impl kani::Arbitrary for LookupTableMeta {
+    fn any() -> Self {
+        Self {
+            deactivation_slot: kani::any::<u64>(),
+            last_extended_slot: kani::any::<u64>(),
+            last_extended_slot_start_index: kani::any::<u8>(),
+            authority: Some(*kani_new_pubkey()),
+            _padding: kani::any::<u16>(),
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Eq, Clone, BorshDeserialize, BorshSerialize)]
 pub struct AddressLookupTable<'a> {
     pub meta: LookupTableMeta,
     pub addresses: Cow<'a, [Pubkey]>,
+}
+
+#[cfg(any(kani, feature = "kani"))]
+impl<'a> kani::Arbitrary for AddressLookupTable<'a> {
+    fn any() -> Self {
+        Self {
+            meta: kani::any::<LookupTableMeta>(),
+            addresses: Cow::from(vec![*kani_new_pubkey()]),
+        }
+    }
 }

d66d702e87712268a3626b245ad35d7b02318c6d
swaroop-osec <swaroop@osec.io>
2025-02-18 20:34:54 +0530

    Handle result type

--- macro_core/src/stub.rs
+++ macro_core/src/stub.rs
@@ -7,10 +7,27 @@ pub fn stub(attr: TokenStream, item: TokenStream) -> TokenStream {
     let return_expr = syn::parse2::<Expr>(attr).unwrap();
     let mut input_fn = syn::parse2::<ItemFn>(item).unwrap();
 
+    // Check if the return type is a Result
+    let is_result_type = if let syn::ReturnType::Type(_, ty) = &input_fn.sig.output {
+        if let syn::Type::Path(type_path) = &**ty {
+            type_path.path.segments.last().unwrap().ident == "Result"
+        } else {
+            false
+        }
+    } else {
+        false
+    };
+
     // Replace the function body with a return statement
-    input_fn.block = syn::parse_quote!({
-        return #return_expr;
-    });
+    input_fn.block = if is_result_type {
+        syn::parse_quote!({
+            return Ok(#return_expr);
+        })
+    } else {
+        syn::parse_quote!({
+            return #return_expr;
+        })
+    };
 
     // Generate the new function with the modified body
     let output = quote! {
